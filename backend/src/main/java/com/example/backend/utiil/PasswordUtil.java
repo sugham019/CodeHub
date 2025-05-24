e24fd755279c0fa05bae0212db266936e9f3e493
sugham019 <sughamkharel123@gmail.com>
2025-05-24 15:18:57 +0545

    Add util

---
+++ backend/src/main/java/com/example/backend/utiil/PasswordUtil.java
@@ -0,0 +1,18 @@
+package com.example.backend.utiil;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtil {
+
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+
+    public static String hashPassword(String password){
+        return passwordEncoder.encode(password);
+    }
+
+    // Todo
+    public static boolean isPasswordStrong(String password){
+        return true;
+    }
+
+}
