224d54b9c2b7a8e706e82b530b59964ef2738218
sugham019 <sughamkharel123@gmail.com>
2025-07-26 16:02:02 +0545

    update

--- src/main/java/com/example/backend/service/CodeService.java
+++ src/main/java/com/example/backend/service/CodeService.java
@@ -3,8 +3,15 @@ package com.example.backend.service;
 import com.example.backend.dto.CodeSubmitDto;
 import com.example.backend.dto.CodeResultDto;
 
-public interface CodeService {
+public abstract class CodeService {
 
-    CodeResultDto submit(CodeSubmitDto codeInput);
+    public abstract CodeResultDto compileAndRun(CodeSubmitDto codeInput);
+
+    public void validateSubmittedCode(CodeSubmitDto codeSubmitDto){
+        if(codeSubmitDto.getInputs().length != codeSubmitDto.getExpectedOutputs().length){
+            throw new RuntimeException("Test Case Error: Inputs length and Expected Output length is not equal");
+        }
+        // Todo
+    }
 
 }
